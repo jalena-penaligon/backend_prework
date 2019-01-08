f0239af7ed5090ecf7c828ffa1b6b2925d661149
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-01-07 21:49:43 -0700

    Updates Day 7 Work

--- day_7/10_little_monkeys.rb
+++ day_7/10_little_monkeys.rb
@@ -1,18 +1,46 @@
+puts "Enter the number of monkeys that you would like to jump on the bed."
 
-puts "How many monkeys would you like to jump on the bed?"
-number = gets.chomp.to_i
+x = gets.chomp.to_i
 
-
-while number >= 2
-  puts "#{number} little monkeys jumping on the bed,
-One fell off and bumped his head,
-Mama called the doctor and the doctor said,
-'No more monkeys jumping on the bed!'
-  --------------------"
-  number = number - 1
+if x == 0
+  abort("Sorry, that's an incorrect value. Please enter a number.")
+elsif x > 20
+  abort("Yikes! That's too many monkeys. Try a number between 1-20.")
 end
 
-puts "#{number} little monkey jumping on the bed,
-One fell off and bumped his head,
-Mama called the doctor and the doctor said,
-'Get those monkeys right to bed!'"
+monkeys = {
+  1 => 'One',
+  2 => 'Two',
+  3 => 'Three',
+  4 => 'Four',
+  5 => 'Five',
+  6 => 'Six',
+  7 => 'Seven',
+  8 => 'Eight',
+  9 => 'Nine',
+  10 => 'Ten',
+  11 => 'Eleven',
+  12 => 'Twelve',
+  13 => 'Thirteen',
+  14 => 'Fourteen',
+  15 => 'Fifteen',
+  16 => 'Sixteen',
+  17 => 'Seventeen',
+  18 => 'Eighteen',
+  19 => 'Nineteen',
+  20 => 'Twenty',
+}
+
+ while x >= 2
+   puts "#{monkeys[x]} little monkeys jumping on the bed,
+ One fell off and bumped his head,
+ Mama called the doctor and the doctor said,
+'No more monkeys jumping on the bed!'
+  --------------------"
+  x = x - 1
+ end
+
+puts "One little monkey jumping on the bed,
+ One fell off and bumped his head,
+ Mama called the doctor and the doctor said,
+ 'Get those monkeys right to bed!'"
--- day_7/fizzbuzz.rb
+++ day_7/fizzbuzz.rb
@@ -1,6 +1,11 @@
 
 puts "Enter a number to see a mathematical FizzBuzz trick!"
 y = gets.chomp.to_i
+
+if y == 0
+  puts "Sorry, that's an incorrect value. Please enter a number."
+end
+
 x = 1
 
 while x <= y
